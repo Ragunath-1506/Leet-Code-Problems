deb269723e45eff6ae7b956d8e0de59e3b979c90
Ragunath-1506 <ragunathmuthukaruppan@gmail.com>
2026-07-24 10:15:48 +0530

    Time: 4 ms (100%), Space: 46 MB (33.79%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0){
+            return false;
+        }
+        int copy=x;
+        int rev=0;
+        while (x>0){
+            rev=rev*10+x%10;
+            x/=10;
+        }
+        if(rev == copy){
+            return true;
+        }
+        return false;
+
+    }
+}
