4ef90a1d4479ca45ab791ec298b0a456c0b1f85c
Ragunath-1506 <ragunathmuthukaruppan@gmail.com>
2026-07-30 23:12:22 +0530

    Time: 3 ms (86.12%), Space: 42.3 MB (99.88%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c :s.toCharArray()){
+            if (c == '(' ){
+                st.push(')');
+            }
+            else if(c=='['){
+                st.push(']');
+            }
+            else if(c=='{'){
+                st.push('}');
+            }
+            else{
+                if (st.isEmpty() || st.peek() != c) {
+                    return false;
+                }
+                st.pop();
+            }
+
+        }
+        if(st.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
