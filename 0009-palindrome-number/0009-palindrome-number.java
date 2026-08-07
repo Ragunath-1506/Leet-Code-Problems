f1ae572cac3f26ef561a26a863034264e1fffd7e
Ragunath M <ragunathmuthukaruppan@gmail.com>
2026-08-07 09:52:01 +0530

    Time: 5 ms (81.26%), Space: 45.7 MB (94.72%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,18 +1,21 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x<0){
-            return false;
-        }
-        int copy=x;
-        int rev=0;
-        while (x>0){
-            rev=rev*10+x%10;
-            x/=10;
-        }
-        if(rev == copy){
-            return true;
-        }
+       if (x<0){
         return false;
+       }
+       int copy = x;
+       int rev = 0;
+       while(x!=0){
+        rev=rev*10+x%10;
+        x=x/10;
+       }
+       if(rev == copy){
+        return true;
+       }
+       else{
+        return false;
+       }
+       
 
     }
 }
